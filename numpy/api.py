08f8e488f7db670493e02afdbc9a97642844bf74
Pensiveant <1429055703@qq.com>
2020-07-10 11:17:30 +0800

    numpy示例更新

--- numpy/api.py
+++ numpy/api.py
@@ -1,8 +1,38 @@
 import numpy as np
 
-baseArray=np.array([[1,2,3,4],[5,6,7,8]])
-item=baseArray.item(5)
-print(item)
+# baseArray=np.array([[1,2,3,4],[5,6,7,8]])
+# item=baseArray.item(5)
+# print(item)
 
-listArray=baseArray.tolist()
-print(listArray)
+# listArray=baseArray.tolist()
+# print(listArray)
+
+
+# # empty(shape, dtype=float, order='C')
+# emptyArray=np.empty([2,3]) # 2行3列
+# emptyArray1=np.empty((2,3),dtype='int')
+# emptyArray2=np.empty((2,3),dtype='int',order='F')
+# print(emptyArray1)
+# print(emptyArray2)
+
+# # empty_like(prototype, dtype=None, order='K', subok=True, shape=None)
+# a=[[1,2,3],[4,5,6]]
+# emptLikeArr=np.empty_like(a)
+# emptLikeArr1=np.empty_like(a,dtype=float) # 覆盖给定数组的类型int为float
+# emptLikeArr2=np.empty_like(a,shape=(1,3)) # 覆盖给定数组的形状(2,3)为(1,3)
+
+# # numpy.eye(N, M=None, k=0, dtype=<class 'float'>, order='C')[source]
+# eyeArr=np.eye(3) # (3,3)
+# eyeArr1=np.eye(3,4) # (3,4)
+# eyeArr2=np.eye(3,k=1) # 对角线索引为1
+# eyeArr3=np.eye(3,k=-1) # 对角线索引为-1
+# print(eyeArr2)
+
+# # numpy.identity(n, dtype=None)[source]
+# identityArr=np.identity(3,dtype=complex)
+# print(identityArr)
+
+# numpy.ones(shape, dtype=None, order='C')
+onesArr=np.ones((2,3,4))
+onesArr1=np.ones((3,4),dtype=complex)
+print(onesArr1)
--- numpy/createArray.py
+++ numpy/createArray.py
@@ -1,21 +1,26 @@
+'''
+# 创建ndarray对象
+'''
+
+
 import numpy as np
 
-# list创建Numpy array
-listToArray=np.array([1,3,4,5])
-print(listToArray)
-print(listToArray.dtype)
-listToArray1=np.array([1.1,2.3,4.3])
-print(listToArray1)
-print(listToArray1.dtype)
+# # list创建Numpy array
+# listToArray=np.array([1,3,4,5])
+# print(listToArray)
+# print(listToArray.dtype)
+# listToArray1=np.array([1.1,2.3,4.3])
+# print(listToArray1)
+# print(listToArray1.dtype)
 
 
-# tuple创建Numpy array
-tupleToArray=np.array([(1,3),(4,5)])
-print(tupleToArray)
+# # tuple创建Numpy array
+# tupleToArray=np.array([(1,3),(4,5)])
+# print(tupleToArray)
 
-# 创建时指定数据类型
-specifiedType=np.array( [ [1,2], [3,4] ], dtype=complex )
-print(specifiedType)
+# # 创建时指定数据类型
+# specifiedType=np.array( [ [1,2], [3,4] ], dtype=complex )
+# print(specifiedType)
 
 # 元素未知但大小已知
 zerosArray=np.zeros((2,3))
