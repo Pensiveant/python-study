44dbc0393cc4ba0c831c81874568de5dc057c107
Pensiveant <1429055703@qq.com>
2020-07-13 08:57:48 +0800

    总结更新

--- numpy/api.py
+++ numpy/api.py
@@ -1,13 +1,5 @@
 import numpy as np
 
-# baseArray=np.array([[1,2,3,4],[5,6,7,8]])
-# item=baseArray.item(5)
-# print(item)
-
-# listArray=baseArray.tolist()
-# print(listArray)
-
-
 # # empty(shape, dtype=float, order='C')
 # emptyArray=np.empty([2,3]) # 2行3列
 # emptyArray1=np.empty((2,3),dtype='int')
@@ -32,7 +24,43 @@ import numpy as np
 # identityArr=np.identity(3,dtype=complex)
 # print(identityArr)
 
-# numpy.ones(shape, dtype=None, order='C')
-onesArr=np.ones((2,3,4))
-onesArr1=np.ones((3,4),dtype=complex)
-print(onesArr1)
+# # numpy.ones(shape, dtype=None, order='C')
+# onesArr=np.ones((2,3,4))
+# onesArr1=np.ones((3,4),dtype=complex)
+# print(onesArr1)
+
+# # numpy.array(object, dtype=None, *, copy=True, order='K', subok=False, ndmin=0)
+# arrayArr=np.array([1,2,3,4]) # (4,)
+# arrayArr1=np.array([[1,2,3,4],[5,6,7,8]]) # (2,4)
+# arrayArr2=np.array([[1,2,3,4],[5,6,7,8]],dtype=float,ndmin=3)
+# print(arrayArr2)
+
+
+# # numpy.asarray(a, dtype=None, order=None)
+# asarrayArr=np.asarray([1,3,4,5])
+# asarrayArr1=np.asarray([(1,2),(3,4)])
+# asarrayArr2=np.asarray(((1,2),(3,4)))
+# asarrayArr3=np.asarray(([1,2],[3,4]),order='C')
+# print(asarrayArr3)
+
+
+# # numpy.asanyarray(a, dtype=None, order=None)
+# asanyarrayArr=np.asanyarray([1,3,4,5])
+# asanyarrayArr1=np.asanyarray([(1,2),(3,4)])
+# asanyarrayArr2=np.asanyarray(((1,2),(3,4)))
+# asanyarrayArr3=np.asanyarray(([1,2],[3,4]),order='C')
+# print(asanyarrayArr3)
+
+
+# # numpy.ascontiguousarray(a, dtype=None)
+# x = np.arange(6).reshape(2,3)
+# print(x.flags) # x.flags.C_CONTIGUOUS=false
+# ascontiguousarrayArr=np.ascontiguousarray(x)
+# print(x.flags) # x.flags.C_CONTIGUOUS=true
+
+
+# numpy.asmatrix(data, dtype=None)
+
+x=np.array([[1,2],[3,4]])
+m=np.asmatrix(x)
+print(m)
