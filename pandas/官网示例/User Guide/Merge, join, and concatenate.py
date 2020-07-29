da23e9b648eb818dbae43f9d5e759103c13d03ee
Pensiveant <1429055703@qq.com>
2020-07-29 14:53:35 +0800

    示例更新

---
+++ pandas/官网示例/User Guide/Merge, join, and concatenate.py
@@ -0,0 +1,51 @@
+'''
+# Series和DataFrame索引示例
+# @author:pensiveant
+# @reference:https://pandas.pydata.org/docs/user_guide/merging.html#timeseries-friendly-merging
+'''
+
+import pandas as pd
+import numpy as np
+
+# 简单示例
+df1 = pd.DataFrame({
+    'A': ['A0', 'A1', 'A2', 'A3'],
+    'B': ['B0', 'B1', 'B2', 'B3'],
+    'C': ['C0', 'C1', 'C2', 'C3'],
+    'D': ['D0', 'D1', 'D2', 'D3']
+},index=[0,1,2,3])
+
+df2 = pd.DataFrame({
+    'A': ['A4', 'A5', 'A6', 'A7'],
+    'B': ['B4', 'B5', 'B6', 'B7'],
+    'C': ['C4', 'C5', 'C6', 'C7'],
+    'D': ['D4', 'D5', 'D6', 'D7']
+},index=[4,5,6,7])
+
+df3 = pd.DataFrame({
+    'A': ['A8', 'A9', 'A10', 'A11'],
+    'B': ['B8', 'B9', 'B10', 'B11'],
+    'C': ['C8', 'C9', 'C10', 'C11'],
+    'D': ['D8', 'D9', 'D10', 'D11']
+},index=[8,9,10,11])
+
+frames=[df1,df2,df3]
+result=pd.concat(frames)
+print(result)
+
+
+# 使用传递的键作为最外层级别来构造层次结构索引。
+resultOfKeys=pd.concat(frames,keys=['x','y','z'])
+print(resultOfKeys)
+print(resultOfKeys.loc['y'])
+
+# 
+df4=pd.DataFrame({
+    'B':['B2','B3','B6','B7'],
+    'D':['D2','D3','D6','D7'],
+    'F':['F2','F3','F6','F7']
+},index=[2,3,6,7])
+resultOfColumnAxis=pd.concat([df1,df4],axis=1,sort=False)
+resultOfIndexAxis=pd.concat([df1,df4],sort=False)
+print(resultOfColumnAxis)
+print(resultOfIndexAxis)
