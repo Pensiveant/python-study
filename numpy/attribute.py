cbcd77aad14ff205ce112b42e1e2339c0ae2caf9
Pensiveant <1429055703@qq.com>
2020-10-12 09:18:14 +0800

    numpy示例更新

--- numpy/attribute.py
+++ numpy/attribute.py
@@ -6,17 +6,17 @@ import numpy as np
 
 ndarray=np.array([[1,2,3],[4,5,6],[7,8,9]])
 # print(ndarray.flags)
-print(ndarray.shape)
+# print(ndarray.shape)
 # print(ndarray.strides)
 # print(ndarray.ndim)
 # print(ndarray.data)
 # print(ndarray.size)
-# print(ndarray.itemsize)
-# print(ndarray.nbytes)
+# print(ndarray.itemsize) # 4
+# print(ndarray.nbytes) # 36
 # print(ndarray.base)
 # print(ndarray.dtype)
 # print(ndarray.T)
 # print(ndarray.real)
 # print(ndarray.imag)
-# print(ndarray.flat)
+print(ndarray.flat)
 # print(ndarray.ctypes)
