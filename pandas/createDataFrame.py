45a267c9c9aed2bbb72a0912ed5e1950d1f8d7d2
Pensiveant <1429055703@qq.com>
2022-09-01 10:33:06 +0800

    refactor: createDataFrame格式调整

--- pandas/createDataFrame.py
+++ pandas/createDataFrame.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import numpy as np
 
+# ---------列的集合---------------------
 # # 字典：key为字符串，代表列名；值为Series或列表或同长度的一维ndarray对象或字典，代表一列
 # seriesDict={
 #     'one':pd.Series([1.,2.,3.],index=['a','b','c']),
@@ -28,7 +29,7 @@ import numpy as np
 # print(df2)
 
 # dictDict={
-#     'one':{'a':1.,'b':2,'c':3.},    
+#     'one':{'a':1.,'b':2,'c':3.},
 #     'two':{'a':1.,'b':2,'c':3.,'d':4.}
 #     }
 # df3=pd.DataFrame(dictDict)
@@ -51,22 +52,32 @@ import numpy as np
 
 # # 列表：每个元素为Series或列表或同长度的一维ndarray对象或字典，代表一行
 
-# seriesList=[pd.Series([1.,2.,3.],index=['a','b','c']),pd.Series([1.,2.,3.,4.],index=['a','b','c','d'])]
-# df4=pd.DataFrame(seriesList,index=['one','two'])
+# seriesList = [
+#     pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
+#     pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd'])
+# ]
+# df4 = pd.DataFrame(seriesList, index=['one', 'two'])
 # print(df4)
 
-# listList=[[1.,2.,3.,4.], # 默认索引为0,1,2,3...
-#           [4.,3.,2.,1.]]
-# df5=pd.DataFrame(listList,index=['one','two'],columns=['a','b','c','d'])
+# listList = [
+#     [1., 2., 3., 4.],  # 默认索引为0,1,2,3...
+#     [4., 3., 2., 1.]
+# ]
+# df5 = pd.DataFrame(listList, index=['one', 'two'], columns=['a', 'b', 'c', 'd'])
 # print(df5)
 
-# ndarrayList=[np.array([1,2,3,4]), # 默认索引为0,1,2,3...
-#              np.array([7,8,9,10])]
-# df6=pd.DataFrame(ndarrayList,index=['one','two'])
+# ndarrayList = [
+#     np.array([1, 2, 3, 4]),  # 默认索引为0,1,2,3...
+#     np.array([7, 8, 9, 10])
+# ]
+# df6 = pd.DataFrame(ndarrayList, index=['one', 'two'])
 # print(df6)
 
-# dictList=[{'a':1.,'b':2,'c':3.},{'a':1.,'b':2,'c':3.,'d':4.}]
-# df7=pd.DataFrame(dictList,index=['one','two'])
+# dictList = [
+#     {'a': 1., 'b': 2, 'c': 3.},
+#     {'a': 1., 'b': 2, 'c': 3., 'd': 4.}
+# ]
+# df7 = pd.DataFrame(dictList, index=['one', 'two'])
 # print(df7)
 
 # # 2维numpy.ndrray对象
@@ -74,13 +85,13 @@ import numpy as np
 # df8=pd.DataFrame(twoDNdarray,index=['one','two'],columns=['a','b','c'])
 # print(df8)
 
-# # DataFrame
+# # # DataFrame
 # dataFrame=pd.DataFrame(data=[[1,2,3],[4,5,6],[4,5,6]],index=['one','two','tree'],columns=['a','b','c'])
 # print(dataFrame)
 # df9=pd.DataFrame(dataFrame,copy=True)
 # print(df9)
 
-# 字典，成员为元组
+# # 字典，成员为元组
 dictOfTuple={
         ('a', 'b'): {('A', 'B'): 1, ('A', 'C'): 2},
         ('a', 'a'): {('A', 'C'): 3, ('A', 'B'): 4},
